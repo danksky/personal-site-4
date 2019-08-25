3fcd94318e8b1eaf37ea74192152695fe7e7e44f
Daniel Kawalsky <dkawalsk@usc.edu>
2019-08-25 14:59:06 -0500

    trying to serve favicon

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+var favicon = require('serve-favicon');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // API calls
 app.get('/api/hello', (req, res) => {
