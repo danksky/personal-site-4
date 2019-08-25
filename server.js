0a96cc1bb5a2be3d92268648fb7717fda206015a
Daniel Kawalsky <dkawalsk@usc.edu>
2019-08-25 15:08:52 -0500

    new serve library attempt

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-var favicon = require('serve-favicon');
+const favicon = require('express-favicon');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 // API calls
 app.get('/api/hello', (req, res) => {
