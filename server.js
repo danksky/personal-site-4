12bdd2b964808d02555ec9cfec0f5761d7841137
Daniel Kawalsky <dkawalsk@usc.edu>
2019-09-28 23:40:50 -0500

    hopefully favicon shows up now

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(__dirname + '/public/favicon.png'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // API calls
 app.get('/api/hello', (req, res) => {
