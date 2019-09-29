2c136e7fef33538541557791b7cb2ba7da4dfc07
Daniel Kawalsky <dkawalsk@usc.edu>
2019-09-28 23:52:12 -0500

    hopefully doesn't crash this time?

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(favicon(__dirname + '/public/favicon.png'));
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
 
 // API calls
 app.get('/api/hello', (req, res) => {
