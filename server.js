7f18dfa9cf68b9a4600450b226a9597a7b01d04f
Daniel Kawalsky <dkawalsk@usc.edu>
2019-09-28 23:48:14 -0500

    added both favicon .ico and .png bc crashed

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 // API calls
 app.get('/api/hello', (req, res) => {
